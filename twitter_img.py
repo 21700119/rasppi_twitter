8ceff01c540408870df138088ef5c3acf1786101
21700119 <59634108+21700119@users.noreply.github.com>
2020-06-07 00:17:12 +0900

    Update twitter_img.py

--- twitter_img.py
+++ twitter_img.py
@@ -15,7 +15,7 @@ twitter = Twython(
 
 message = "Hello World - here's a picture!"
 
-image = open('orange.jpeg', 'rb')
+image = open('rasppi.jpeg', 'rb')
 response = twitter.upload_media(media=image)
 media_id = [response['media_id']]
 twitter.update_status(status=message, media_ids=media_id)
